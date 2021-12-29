ff3e9728e3cdb12376d765752023946a2ee4aad6
susanmarie87 <susan.marie87@gmail.com>
2021-12-29 22:58:17 +0000

    Fixed formatting in json files. changed bag to checkout in templates

--- checkout/models.py
+++ checkout/models.py
@@ -20,10 +20,10 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def _generate_order_number(self):
         """
--- products/models.py
+++ products/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
     def __str__(self):
         return self.name
