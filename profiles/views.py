5d43e6a151ac9762776a3f52cfdb01f3b7b9c78b
susanmarie87 <susan.marie87@gmail.com>
2022-01-10 05:05:28 +0000

    Add urls, views templates for profile. Updated allauth files.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """Displays profile"""
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
