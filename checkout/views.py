df703a500a57b9d07628bcc6db3d77f70f6721a8
susanmarie87 <susan.marie87@gmail.com>
2022-01-12 22:11:57 +0000

    Add login and imports to checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
 
@@ -6,16 +7,19 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
+from profiles.forms import UserProfile
 
 import stripe
 
 
+@login_required(login_url="/accounts/login")
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
+
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -27,9 +31,10 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
