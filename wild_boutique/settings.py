c7861e165debfb3ea3cc72c3730354cae138920a
susanmarie87 <susan.marie87@gmail.com>
2022-01-15 18:48:33 +0000

    Begin deployment

--- wild_boutique/settings.py
+++ wild_boutique/settings.py
@@ -4,6 +4,7 @@ import os
 from os import path
 if os.path.exists('env.py'):
     import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
