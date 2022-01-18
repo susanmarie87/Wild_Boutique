def3a63c8fcf59f6613f5941f6500245520dc579
susanmarie87 <susan.marie87@gmail.com>
2022-01-18 06:57:54 +0000

    Add cache control

--- wild_boutique/settings.py
+++ wild_boutique/settings.py
@@ -185,6 +185,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheCotrol': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'wild-indigos.herokuapp.com'
     AWS_S3_REGION_NAME = 'us-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
