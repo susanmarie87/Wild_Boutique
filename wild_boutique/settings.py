f24af0bdf89768ddbe6f4318cd507c6e9a50d314
susanmarie87 <susan.marie87@gmail.com>
2022-01-18 04:38:48 +0000

    Changed Authorized Host in settings.py

--- wild_boutique/settings.py
+++ wild_boutique/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smp8780-wild-indigos.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['wild-indigos.herokuapp.com', 'localhost']
 
 
 # Application definition
