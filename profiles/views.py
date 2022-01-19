255848ef48be0e24625b18fc7df3e7cb5df8a6ac
susanmarie87 <susan.marie87@gmail.com>
2022-01-19 01:42:37 +0000

    Corrected spelling error on profile views

--- profiles/views.py
+++ profiles/views.py
@@ -33,7 +33,7 @@ def profile(request):
 
 
 def order_history(request, order_number):
-    order = get_object_or_404(Order, order_number=Ornder_number)
+    order = get_object_or_404(Order, order_number=Order_number)
 
 
     messages.info(request, (
