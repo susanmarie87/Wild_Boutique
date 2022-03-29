1200634ed3941c20b68176b43052a7d8358e6692
susanmarie87 <susan.marie87@gmail.com>
2022-03-29 04:03:29 +0000

    Add imports to file and setup api and secret keys.

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,5 +1,20 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
 def webhook(request):
-       payload = request.body
+    """Listen for webhooks from Stripe"""
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # Get the webhook data and verify its signature
+    payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
