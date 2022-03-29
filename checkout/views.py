9a69cb876d875584599ec2d94f3eba52acf06289
susanmarie87 <susan.marie87@gmail.com>
2022-03-29 15:55:53 +0000

    Add view to save customer information.

--- checkout/views.py
+++ checkout/views.py
@@ -3,20 +3,39 @@ from django.shortcuts import (
     render, redirect, reverse, get_object_or_404,
     HttpResponse
 )
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 
 import stripe
+import json
 
 from bag.contexts import bag_contents
 from products.models import Product
 from profiles.forms import UserProfile
+from profiles.forms import UserProfileForm
 from .forms import OrderForm
 
 from .models import Order, OrderLineItem
 
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, ('Sorry, your payment cannot be '
+                                 'processed right now. Please try '
+                                 'again later.'))
+    return HttpResponse(content=e, status=400)           
 
 @login_required(login_url="/accounts/login")
 def checkout(request):
