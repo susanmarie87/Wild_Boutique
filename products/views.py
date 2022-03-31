a41d3e922ce088a7f959a97479b12e5311d32606
susanmarie87 <susan.marie87@gmail.com>
2022-03-31 15:26:40 +0000

    Edit product view corrected to only update, not create, product.

--- products/views.py
+++ products/views.py
@@ -72,15 +72,11 @@ def add_product(request):
 @login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            product = form.save()
+            form.save()
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
