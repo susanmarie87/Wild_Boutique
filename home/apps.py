0ed804d0cf5d802cde1acdf1e6b23563f71cb44d
susanmarie87 <susan.marie87@gmail.com>
2022-04-03 22:21:58 +0000

    Correct python errors.

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,8 @@
+"""Application Configuration"""
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """Instance of app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,4 @@
+"""Url's for home app"""
 from django.urls import path
 from . import views
 
